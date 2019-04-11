16bebe39c8f3c223643bd5a20043137423eba90d
aleant93 <aless.antelmi@gmail.com>
2019-04-11 16:45:42 +0200

    travis

--- src/MyTestUD.jl
+++ src/MyTestUD.jl
@@ -8,7 +8,13 @@ export greet2
 greet() = println("Hello World!")
 greet2() = println("Hello World!")
 
-function add_two(a,b)
+"""
+    add_two(a,b)
+
+Add two numbers: `a` is added once while `b`
+is added twice
+"""
+function add_two(a::Float64,b::Float64)
     a+2b
 end
 
